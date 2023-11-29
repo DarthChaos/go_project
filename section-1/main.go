4f212b3c3a4939b5da057fc6f8234cb4c0ac2875
DarthChaos <oniricmemories@gmail.com>
2023-11-29 15:34:46 -0500

    strings

--- section-1/main.go
+++ section-1/main.go
@@ -1,24 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func main() {
-	// different vars in same block with same type
-	// var (
-	// 	m1 = 2
-	// 	m2 = 3
-	// )
-	// var instance
-	// var m1 int
-	// m1 = 2
-	// var conversion
-	// var m1 int32
-	// var m2 int64
-	// fmt.Println(int64(m1) + m2)
-	
-	m1 := 2
-	m2 := 3
+func main () {
+	m1 := "Hello World!"
+	m2 := "String: "
 
-	// fmt.Println("Hello World!")
-	fmt.Println(m1 + m2)
+	m1 = "Another value"
+
+	fmt.Println(m2 + m1, strings.Split(m1, " "), strings.Contains(m1, m2), len(m1))
 }
+
+// func main() {
+// 	// different vars in same block with same type
+// 	// var (
+// 	// 	m1 = 2
+// 	// 	m2 = 3
+// 	// )
+// 	// var instance
+// 	// var m1 int
+// 	// m1 = 2
+// 	// var conversion
+// 	// var m1 int32
+// 	// var m2 int64
+// 	// fmt.Println(int64(m1) + m2)
+	
+// 	m1 := 2
+// 	m2 := 3
+
+// 	// fmt.Println("Hello World!")
+// 	fmt.Println(m1 + m2)
+// }
