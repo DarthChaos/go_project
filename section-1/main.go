49683582e1f6c89c969638fcd7ca8b64a1747f2d
DarthChaos <oniricmemories@gmail.com>
2023-11-29 15:23:20 -0500

    variables in main

--- section-1/main.go
+++ section-1/main.go
@@ -3,7 +3,22 @@ package main
 import "fmt"
 
 func main() {
+	// different vars in same block with same type
+	// var (
+	// 	m1 = 2
+	// 	m2 = 3
+	// )
+	// var instance
+	// var m1 int
+	// m1 = 2
+	// var conversion
+	// var m1 int32
+	// var m2 int64
+	// fmt.Println(int64(m1) + m2)
 	
+	m1 := 2
+	m2 := 3
 
-	fmt.Println("Hello World!")
+	// fmt.Println("Hello World!")
+	fmt.Println(m1 + m2)
 }
